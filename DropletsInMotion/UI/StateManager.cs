3b6bd22198f92401aac47f84bd32ffed68ca5e7c
Joel M. <joel.madsen10@gmail.com>
2024-10-01 11:29:31 +0200

    Update StateManager.cs

--- DropletsInMotion/UI/StateManager.cs
+++ DropletsInMotion/UI/StateManager.cs
@@ -126,6 +126,7 @@ namespace DropletsInMotion.UI
 
             // TODO: Maybe the compiler should return a status code for the compilation.
             IExecutionEngine executionEngine = _serviceProvider.GetRequiredService<IExecutionEngine>();
+            executionEngine.Agents.Clear(); // TODO: We need to clear this because of persistance
             await executionEngine.Execute(listener.Commands, listener.Droplets, _userService.PlatformPath);
 
 
